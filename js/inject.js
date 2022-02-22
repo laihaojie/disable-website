04a7764eec754aa0f054c9025741f7ca2200d4e1
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-02-22 12:23:15 +0800

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -1,13 +1,15 @@
-// document.body.childNodes.forEach(node => {
-//     document.body.removeChild(node);
-// })
-// document.body.remove();
-setTimeout(() => {
-    Array.from(document.body.childNodes).forEach(node => {
-        node.remove()
-    })
-    var div = document.createElement("div")
-    document.body.appendChild(div);
-    div.innerText = "好好学习, 不看一些不相干的东西."
-    div.style = "width:100vw;height:100vh;display:flex;align-items:center;justify-content:center;color:red;font-size:100px;font-weight:bold"
-}, 1000);
+var div = document.createElement("div")
+document.body.appendChild(div);
+div.innerText = "好好学习, 不看一些不相干的东西."
+div.style = "position:fixed;top:0;left:0;bottom:0;right:0;background-color:#ffffff;z-index:9999999;display:flex;align-items:center;justify-content:center;color:red;font-size:100px;font-weight:bold;"
+
+
+// setTimeout(() => {
+//     Array.from(document.body.childNodes).forEach(node => {
+//         node.remove()
+//     })
+//     var div = document.createElement("div")
+//     document.body.appendChild(div);
+//     div.innerText = "好好学习, 不看一些不相干的东西.1"
+//     div.style = "width:100vw;height:100vh;display:flex;align-items:center;justify-content:center;color:red;font-size:100px;font-weight:bold"
+// }, 1000);
